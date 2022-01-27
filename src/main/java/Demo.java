f3459470b335114c8fbb82f2960697d5e5dabebc
fuwenxin <1281370249@qq.com>
2022-01-27 21:02:58 +0800

    feat:localdate使用,

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,5 +1,10 @@
 import stream.RoleType;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.function.Function;
 
@@ -10,7 +15,30 @@ import java.util.function.Function;
 public class Demo {
 
     public static void main(String[] args) {
-          test();
+        Scanner scanner1 = new Scanner(System.in);
+        int year = scanner1.nextInt();
+        Scanner scanner2 = new Scanner(System.in);
+        int month = scanner2.nextInt();
+        Scanner scanner3 = new Scanner(System.in);
+        int day = scanner3.nextInt();
+        LocalDate of = LocalDate.of(year, month, day);
+        int value = of.getDayOfWeek().getValue();
+        int dayOfMonth = of.with(TemporalAdjusters.lastDayOfMonth()).getDayOfMonth();
+        int aa = dayOfMonth%7;
+        HashMap<Object, Object> map = new HashMap<>();
+        for (int i =0;i<7;i++){
+            if (value==0){
+                value=7;
+            }
+            if (i<aa){
+                map.put(value,5);
+            }else {
+                map.put(value,4);
+            }
+            System.out.println(value);
+            value = (value-1)%7;
+            }
+        System.out.println(map.toString());
     }
 
     private static void test() {
